914bb987ef05d0a3ca83d93ea99527c3e0de7f30
xnhao <xnhao@iflytek.com>
2019-04-26 13:03:05 +0800

    无状态的todoListUI

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import {Input, Button, List} from 'antd';
+import React from 'react';
+import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
-  render() {
-    return (
-      <div style={{margin: '10px'}}>
-        <Input placeholder="Basic usage" 
-          style={{width: '300px', marginRight:'10px'}}
-          value={this.props.inputValue}
-          onChange={this.props.InputChange}/>
-        <Button type="primary" onClick={this.props.addItem}>提交</Button>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          size="small"
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (<List.Item onClick={(index) => {this.props.deleteItem(index)}} > {item}</List.Item>)}
-        /> 
-      </div>
-    )
-      
-  }
+const TodoListUI = (props) => {
+  return (
+    <div style={{ margin: '10px' }}>
+      <Input placeholder="Basic usage"
+        style={{ width: '300px', marginRight: '10px' }}
+        value={props.inputValue}
+        onChange={props.InputChange} />
+      <Button type="primary" onClick={props.addItem}>提交</Button>
+      <List
+        style={{ marginTop: '10px', width: '300px' }}
+        size="small"
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (<List.Item onClick={(index) => { props.deleteItem(index) }} > {item}</List.Item>)}
+      />
+    </div>
+  )
 }
 export default TodoListUI
